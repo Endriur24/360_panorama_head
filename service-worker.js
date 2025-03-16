4619cc5b467e8b579f4282707b2bdcfb817506f7
Andrzej <37420882+Endriur24@users.noreply.github.com>
2025-03-16 19:41:24 +0100

    remove name filter

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Cache name with version - increment this version to trigger the update process
-const CACHE_NAME = 'panohead-v8';
+const CACHE_NAME = 'panohead-v9';
 
 // List of assets to cache for offline functionality
 const ASSETS = [
